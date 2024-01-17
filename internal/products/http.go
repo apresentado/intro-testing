bd65d9b78470342fdd6b29047042025e57c2f535
apresentado <agustina.presentado@mercadolibre.com>
2024-01-17 00:22:22 -0300

    SIN TERMINAR -  desarrollo del handler

---
+++ internal/products/http.go
@@ -0,0 +1,58 @@
+//handler:
+
+package products
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-delve/delve/service"
+)
+
+//HTTPProductsHandler is a HTTP handler that exposes the products service
+
+type HTTPProductsHandler struct {
+	// service is used to access the products service
+	service Service
+}
+
+// NewHTTPProductsHandler creates a new instance of the HTTPProductsHandler
+
+func NewHTTPProductsHandler(service Service) *HTTPProductsHandler {
+	return &HTTPProductsHandler{
+		service: service,
+	}
+}
+
+// GetByID handles a HTTP request to get a product by its ID
+//reciVE a product id as a path paramether (:id)
+func (h *HTTPProductsHandler) GetByID(ctx *gin.Context) {
+	// get the product id from the path parameter
+	productID, err := strconv.Atoi(ctx.Param("id"))
+	// get the product from the service
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid product id",
+		})
+		return
+	}
+	//delegar la llamada al servicio
+	product, err := h.service.GetByID(ctx, productID)
+
+	if err != nil {
+		switch err {
+		case errNotFound:
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error": "product not found",
+			})
+		default:
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "an unexpected error occurred",
+			})
+	}
+	return
+	}
+	// return the product as a JSON response
+	ctx.JSON(http.StatusOK, product)
+}
